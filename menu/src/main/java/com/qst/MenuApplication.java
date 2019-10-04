de5db90d6d769bbae0ab399efd9727cdaa230d8f
DELL <295489822@qq.com>
2019-10-04 09:50:36 +0800

    springcloud

--- menu/src/main/java/com/qst/MenuApplication.java
+++ menu/src/main/java/com/qst/MenuApplication.java
@@ -4,6 +4,7 @@ package com.qst;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
 @MapperScan("com.qst.mapper")
